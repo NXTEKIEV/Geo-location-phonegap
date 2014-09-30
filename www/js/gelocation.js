9914cef39aaccfb0157dd09875c5f550effe21ba
Aleksei <aleksei@nxte.nl>
2014-09-30 13:16:49 +0300

    first commit

---
+++ www/js/gelocation.js
@@ -0,0 +1,35 @@
+// Wait for Cordowa to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+//Cordova is ready
+//
+function onDeviceReady() {
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Geolocation
+//
+function onSuccess(position){
+  var div = document.getElementById('geo_loc');
+  div.innerHTML = 'Latitude:'           +  position.coords.latitude         +  '<br>' +
+                  'Longitude:'          +  position.coords.longitude        +  '<br>' +
+                  'Altitude:'           +  position.coords.altitude         +  '<br>' +
+                  'Accuracy:'           +  position.coords.accuracy         +  '<br>' +
+                  'Altitude Accuracy:'  +  position.coords.altitudeAccuracy +  '<br>' +
+                  'Heading:'            +  position.coords.heading          +  '<br>' +
+                  'Speed:'              +  position.coords.speed            +  '<br>' +
+                  'Timestamp:'          +  position.timestamp               + '<br>';
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+  alert('code: '    + error.code    + '\n' +
+  'message: ' + error.message + '\n');
+}
+
+function getlocation(){
+  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+}
